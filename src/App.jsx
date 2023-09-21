54349e2b5afe80ed37056037590ed9af6c2dc485
Miguel Angel Jurado <majg1987@hotmail.com>
2023-09-21 21:21:40 +0200

    create basic board

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
-
+import { useState } from 'react'
+import Square from './components/Square'
 import './App.css'
 
 function App() {
+  const [board, setBoard] = useState(Array(9).fill(null))
+
 
   return (
-    <>
-
-    </>
+    <main className='w-fit my-10 mx-auto'>
+      <h1 className='text-color1 text-3xl font-croissant text-center'>
+       Tres en Raya
+      </h1>
+      <section className='grid grid-cols-3 gap-2.5 
+                          my-10'
+      >
+        {
+          board.map((square, index) => {
+            return(
+              <Square
+                key={index}
+                index={index}
+              >
+                { square } 
+              </Square>
+            )
+          })
+        }
+      </section>
+    </main>
   )
 }
 
