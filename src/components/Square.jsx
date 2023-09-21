93269c09f6439db58526505b2de9d460ef478a76
Miguel Angel Jurado <majg1987@hotmail.com>
2023-09-21 21:13:35 +0200

    create basic square

---
+++ src/components/Square.jsx
@@ -0,0 +1,15 @@
+
+const Square = () => {
+    return (       
+            <div className="w-28 h-28  
+                            border-4 border-color1 
+                            rounded-3xl
+                            text-color1 text-5xl
+                            grid place-items-center" 
+            >
+                
+            </div>
+    );
+}
+
+export default Square;
