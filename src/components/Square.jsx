3049075fed8928c828d67fcb283e929b3ebef3c0
Miguel Angel Jurado <majg1987@hotmail.com>
2023-09-24 18:51:29 +0200

    create square

--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,13 +1,19 @@
 
-const Square = () => {
+const Square = ( { children, updateBoard, index }) => {
+    
+    const handleClick = () => {
+        updateBoard(index)
+    }
+    
     return (       
             <div className="w-28 h-28  
                             border-4 border-color1 
                             rounded-3xl
                             text-color1 text-5xl
                             grid place-items-center" 
+                onClick={handleClick}
             >
-                
+                { children }   
             </div>
     );
 }
